0bf81c05556c61b1dbf5b3d88172969c0386179f
sinytim <sinytim@mail.ru>
2019-01-10 17:35:38 +0300

    First program.

--- SamyakDattaBook/main.cpp
+++ SamyakDattaBook/main.cpp
@@ -1,6 +1,15 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
 
 
 int main() {
+
+    cv::Mat image = cv::imread("images/lena.jpg", cv::IMREAD_COLOR);
+
+    cv::imshow("Image", image);
+
+    cv::waitKey();
+
     return 0;
 }
